b1617277eb694b6c429f1b6be97f368879edb724
sarah-svg <kjskillen@gmail.com>
2021-02-22 15:28:12 -0800

    second reducer test for search

--- src/reducer/gitUserReducer.test.js
+++ src/reducer/gitUserReducer.test.js
@@ -1,10 +1,11 @@
 import { findUser } from '../actions/FindUser';
+import { searchUser } from '../actions/searchUser';
 import reducer from './gitUserReducer';
 
 describe('git user reducer', () => {
-  it('checks to make sure the reducer is working', () => {
+  it('checks to make sure the reducer is working with FIND_USER', () => {
     const state = {
-      users: [],
+      users: {},
       search: '',
       repos: []
     };
@@ -19,4 +20,21 @@ describe('git user reducer', () => {
       search: '',
       repos: [] });
   });
-});
+  it('checks to make sure the SEARCH_USER is functioning', () => {
+    const state = {
+      users: {},
+      search: '',
+      repos: []
+    };
+
+    const action = searchUser('Sarah');
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      users: {},
+      search: 'Sarah',
+      repos: []
+    });
+
+  }); });
