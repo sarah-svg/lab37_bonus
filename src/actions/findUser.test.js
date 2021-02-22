17924b71403051bc4da70333980c7af04fbeebd7
sarah-svg <kjskillen@gmail.com>
2021-02-21 21:21:23 -0800

    wrote test and its passing on the action

---
+++ src/actions/findUser.test.js
@@ -0,0 +1,14 @@
+import { findUser, FIND_USER } from './FindUser';
+
+describe('find user action', () => {
+  it('finds a specific user', () => {
+
+    const action = findUser({
+      users: 'sarah-svg'
+    });
+    expect(action).toEqual({
+      type: FIND_USER,
+      payload: { users: 'sarah-svg' }
+    });
+  });
+});
