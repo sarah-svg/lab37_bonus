ea1bc329b5070364d44b97744e9ec5a2123d8d11
sarah-svg <kjskillen@gmail.com>
2021-02-22 15:58:22 -0800

    test finally passing for the list

--- src/components/gitUser/GitLIst.jsx
+++ src/components/gitUser/GitLIst.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { getUser } from '../../selector/user';
-import uuid from 'react-uuid';
-import GitUser from './GitUser';
 
 const GitList = () => {
-  const users = useSelector(getUser);
-  const allUser = users.map((user) => { 
-    <li key={uuid()}>
-      <GitUser {...user}/>
-    </li>;});
-  return <ul>{allUser}</ul>;
+  const repoList = useSelector(state => state.repos);
+
+  const repoElements = repoList.map(repo => {
+    return <div data-testid="user-div" key={repo.id}>
+      <h3>Repo Name: {repo.name}</h3>
+      <a href={repo.url}>Repo Url: {repo.url}</a>
+    </div>;
+  });
+
+  return (
+    <ul data-testid="repo-list">
+      {repoElements}
+    </ul>
+  );
+};
+
+GitList.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 };
 
 export default GitList;
