72fb754c52fba47db639a88fa0552aaeafb43f81
sarah-svg <kjskillen@gmail.com>
2021-02-21 20:55:01 -0800

    bootstrap and added the fetch to github

---
+++ src/components/gitUser/GitUser.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function GitUser(props) {
+  return (
+    <div>
+            
+    </div>
+  );
+}
+
+GitUser.propTypes = {
+
+};
+
+export default GitUser;
+
