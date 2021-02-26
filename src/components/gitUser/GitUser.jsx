f5bbf4d24f73a6972d7e4b5c7963a1b22cc1e035
sarah-svg <kjskillen@gmail.com>
2021-02-25 21:01:32 -0800

    style

--- src/components/gitUser/GitUser.jsx
+++ src/components/gitUser/GitUser.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import SearchRepos from './searchRepos';
 import styles from './Git.css';
 
 const GitUser = () => {
